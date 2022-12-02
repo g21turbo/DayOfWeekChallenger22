01af1e76f85d192d5505efc7f33e0413e41a1ac2
User <dg21stew@gmail.com>
2022-12-02 00:49:48 -0800

    fixed first method and added second method

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         printDayOfWeek();
+        printDayOfWeek2(2);
 
     }
 
@@ -17,18 +18,35 @@ public class Main {
         int day = scanner.nextInt();
 
         if (day >= 0 && day <= 6) {
-
-            switch (day) {
-                case 0 -> System.out.println("Sunday");
-                case 1 -> System.out.println("Monday");
-                case 2 -> System.out.println("Tuesday");
-                case 3 -> System.out.println("Wednesday");
-                case 4 -> System.out.println("Thursday");
-                case 5 -> System.out.println("Friday");
-                case 6 -> System.out.println("Saturday");
-            }
-        }else {
+            String dayOfWeek = switch (day) {
+                case 0 -> "Sunday";
+                case 1 -> "Monday";
+                case 2 -> "Tuesday";
+                case 3 -> "Wednesday";
+                case 4 -> "Thursday";
+                case 5 -> "Friday";
+                case 6 -> "Saturday";
+                default -> "invalid Number";
+            };
+            System.out.println(day + " stands for the day of " + dayOfWeek);
+        } else {
             System.out.println("Invalid Number");
         }
+    }
+
+    public static void printDayOfWeek2(int day) {
+
+        String dayOfWeek = switch (day) {
+            case 0 -> { yield  "Sunday"; }
+            case 1 -> "Monday";
+            case 2 -> "Tuesday";
+            case 3 -> "Wednesday";
+            case 4 -> "Thursday";
+            case 5 -> "Friday";
+            case 6 -> "Saturday";
+            default -> "Invalid Day";
+
+        };
+        System.out.println(day + " stands for the day of " + dayOfWeek);
     }
 }
